f9e2bfeb10a6e8b4163dd274cec362dfd29a6a89
sonhai <sonhai@bankpay.vn>
2017-06-09 11:03:11 +0700

    aaaa112

--- src/test/java/com/shi/jenkins/AppTest.java
+++ src/test/java/com/shi/jenkins/AppTest.java
@@ -16,7 +16,7 @@ public class AppTest extends TestCase {
 	 */
 	public AppTest(String testName) {
 		super(testName);
-		System.out.println("AppTest(String testName)......................23");
+		System.out.println("AppTest(String testName)......................24");
 	}
 
 	/**
